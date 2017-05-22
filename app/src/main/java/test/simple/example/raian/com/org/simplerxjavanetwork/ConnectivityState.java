f1932bbcd6dbef2d06cd2f9b0fbb42c064e26640
Jonathan Sandoval Cruz <raian@cadaver-2.local>
2017-05-21 21:24:31 -0400

    initial commit

---
+++ app/src/main/java/test/simple/example/raian/com/org/simplerxjavanetwork/ConnectivityState.java
@@ -0,0 +1,21 @@
+package test.simple.example.raian.com.org.simplerxjavanetwork;
+
+/**
+ * Created by raian on 5/21/17.
+ */
+
+public class ConnectivityState {
+
+    public static enum State {
+        CONNECTED,
+        CONNECTING,
+        DISCONNECTED,
+        DISCONNECTING,
+        SUSPENDED,
+        UNKNOWN;
+
+        private State() {
+        }
+    }
+
+}
---
+++ app/src/main/java/test/simple/example/raian/com/org/simplerxjavanetwork/MainActivity.java
@@ -0,0 +1,80 @@
+package test.simple.example.raian.com.org.simplerxjavanetwork;
+
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.github.pwittchen.reactivenetwork.library.rx2.Connectivity;
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable<Integer> integerObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(4);
+                e.onComplete();
+            }
+        });
+
+        Observer<Integer> integerObserver = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext:integer:" + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError:e:" + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+
+        integerObservable.subscribe(integerObserver);
+
+        ReactiveNetwork.observeNetworkConnectivity(this)
+                .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Connectivity>() {
+                    @Override public void accept(final Connectivity connectivity) {
+                        // do something with connectivity
+                        // you can call connectivity.getState();
+                        // connectivity.getType(); or connectivity.toString();
+                        Log.d(TAG, "accept::" + connectivity.getState());
+                        if(String.valueOf(connectivity.getState()).equals(String.valueOf(ConnectivityState.State.DISCONNECTED))) {
+                            Log.d(TAG, "No Connectivity");
+                            Toast.makeText(MainActivity.this, "FCK!!! No connectivity..!!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+}
